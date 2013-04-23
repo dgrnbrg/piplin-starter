feca39b1452e33feec5c9e655da73889dfd61d7c
David Greenberg <dsg123456789@gmail.com>
2013-04-22 22:32:11 -0400

    Move to piplin 0.1.1

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [piplin "0.1.0"]])
+                 [piplin "0.1.1"]])
